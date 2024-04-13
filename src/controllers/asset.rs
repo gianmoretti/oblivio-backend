5435c0504fcbd2e7dfa86ba5ca7d67c5ea4b0189
Gianluca Moretti <gianluca.moretti@dove.it>
2024-04-13 15:53:54 +0200

    Aggiunto metodo per ritorno asset con lista designati

--- src/controllers/asset.rs
+++ src/controllers/asset.rs
@@ -2,15 +2,25 @@
 #![allow(clippy::unnecessary_struct_initialization)]
 #![allow(clippy::unused_async)]
 use loco_rs::prelude::*;
+use sea_orm::{ColumnTrait, QueryFilter};
 use serde::{Deserialize, Serialize};
 
+use crate::models::_entities::asset_designateds::{self, Entity as AssetDesignatedEntity};
 use crate::models::_entities::assets::{ActiveModel, Entity, Model};
+use crate::models::_entities::designateds::{Entity as DesignatedEntity, Model as DesignatedModel};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Params {
     pub name: String,
     pub description: String,
     pub category: String,
+}
+
+// Define a new struct to hold both Asset and its associated Designated entities
+#[derive(Debug, Serialize)]
+pub struct AssetWithDesignated {
+    pub asset: Model,
+    pub designated_list: Vec<DesignatedModel>,
 }
 
 impl Params {
@@ -27,7 +37,40 @@ async fn load_item(ctx: &AppContext, id: i32) -> Result<Model> {
 }
 
 pub async fn list(State(ctx): State<AppContext>) -> Result<Json<Vec<Model>>> {
-    format::json(Entity::find().all(&ctx.db).await?)
+    let assets = Entity::find().all(&ctx.db).await?;
+    format::json(assets)
+}
+
+pub async fn enriched_list(
+    State(ctx): State<AppContext>,
+) -> Result<Json<Vec<AssetWithDesignated>>> {
+    let assets = Entity::find().all(&ctx.db).await?;
+    let mut assets_with_designated = Vec::new();
+
+    for asset in assets {
+        let designated_list = AssetDesignatedEntity::find()
+            .filter(asset_designateds::Column::AssetId.eq(asset.id))
+            .all(&ctx.db)
+            .await?;
+
+        let mut designated_names = Vec::new();
+        for designated in designated_list {
+            let designated_entity = DesignatedEntity::find_by_id(designated.id)
+                .one(&ctx.db)
+                .await?;
+
+            designated_names.push(designated_entity.unwrap());
+        }
+
+        let asset_with_designated = AssetWithDesignated {
+            asset: asset,
+            designated_list: designated_names,
+        };
+
+        assets_with_designated.push(asset_with_designated);
+    }
+
+    format::json(assets_with_designated)
 }
 
 pub async fn add(State(ctx): State<AppContext>, Json(params): Json<Params>) -> Result<Json<Model>> {
@@ -87,4 +130,5 @@ pub fn routes() -> Routes {
         .add("/:id", get(get_one))
         .add("/:id", delete(remove))
         .add("/:id", post(update))
+        .add("/enriched", get(enriched_list))
 }
